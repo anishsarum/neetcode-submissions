89306ca61b774b9fced7798634ef9c53804fa11d
Anish Thapa <anish.thapa@portswigger.net>
2026-09-09 22:53:29 +0100

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        match = {
+            ')':'(',
+            '}':'{',
+            ']':'['
+            }
+        
+        stack = []
+
+        for c in s:
+            if c in match:
+                if not stack or stack[-1] != match[c]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(c)
+        
+        return not stack
